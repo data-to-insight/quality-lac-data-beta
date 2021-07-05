32bd27f05a5db29a025557dd3dac229bd66a4a14
Mark Waddoups <mark.waddoups@socialfinance.org.uk>
2021-07-05 15:07:04 +0100

    Improved visuals of the ChildSelector

--- src/components/ChildSelector.tsx
+++ src/components/ChildSelector.tsx
@@ -9,7 +9,7 @@ interface ChildSelectorProps {
 export default function ChildSelector({ childIds, selected, setSelected }: ChildSelectorProps) {
 
   let rows = [];
-  for (const [i, [childId, num_errors]] of childIds.entries()) {
+  for (const [i, [childId, numErrors]] of childIds.entries()) {
     let selectionTarget: number | null = childId;
     let cellStyle = null;
     
@@ -20,13 +20,16 @@ export default function ChildSelector({ childIds, selected, setSelected }: Child
 
     rows.push(
       <GovUK.Table.Row key={i}>
-        <GovUK.Table.Cell key={i} onClick={() => setSelected(selectionTarget)} style={cellStyle}>{childId} ({num_errors})</GovUK.Table.Cell>
+        <GovUK.Table.Cell key={i} onClick={() => setSelected(selectionTarget)} style={cellStyle}>
+          <div style={{float: 'left', paddingLeft: '10px'}}>{childId}</div>
+          <div style={{float: 'right', fontSize: '0.8em', paddingRight: '10px'}}>{numErrors > 0 ? `${numErrors} errors` : 'OK'}</div>
+        </GovUK.Table.Cell>
       </GovUK.Table.Row>
     );
   }
 
   return (
-    <div style={{'overflowY': 'scroll', 'height': '60vh'}}>
+    <div style={{'overflowY': 'scroll', 'height': '60vh', width: '160px'}}>
       <GovUK.Table style={{fontSize: '1em'}}>
         {rows}
       </GovUK.Table>
