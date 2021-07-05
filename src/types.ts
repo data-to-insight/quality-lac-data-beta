6dfba89d5d50e08ea9c9df39d17bbc602e499b15
Mark Waddoups <mark.waddoups@socialfinance.org.uk>
2021-07-05 13:09:57 +0100

    Added types for errors

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,11 @@
 export type FilesCallback = (arg: UploadedFiles) => void;
-export type DataRow = Map<string, any>
-export type ParsedData = Map<String, Array<DataRow>>
+
+export type DataRow = Map<string, any>;
+export type ParsedData = Map<string, Array<DataRow>>;
+
+export type ErrorCode = string;
+
+export type ErrorIncidences = Map<ErrorCode, Map<string, Array<number>>>;
 
 type UploadedFiles = {description: string, acceptedFiles: Array<File>};
 
